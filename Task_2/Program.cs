a37d29c5e078289ec7ea34a237bddec0273c91d8
SvetlanaMaeva <mystik20009@gmail.com>
2022-07-07 14:52:34 +0300

    Task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,13 +1,25 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-Console.WriteLine("Введи число. Но только число, иначе всё сломается!");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+// [3, 7, 23, 12] -> 19
 
-while (number1 > 0)
+// [-4, -6, 89, 6] -> 0
+
+int[] numberArray = new int[10];
+
+int size = numberArray.Length;
+
+int result = 0;
+
+Console.Write("Массив: ");
+for (int i = 0; i < size; i++)
 {
-    sum = sum + number1 % 10;
-    number1 = number1 / 10;
+    numberArray[i] = new Random().Next(1, 10);
+    
+    if (i % 2 != 0)
+    {
+        result += numberArray[i];
+    }
 }
+Console.WriteLine($"[{String.Join("; ", numberArray)}]");
 
-Console.WriteLine(sum);
+Console.WriteLine($"Cуммa элементов, стоящих на нечётных позициях: {result}");
