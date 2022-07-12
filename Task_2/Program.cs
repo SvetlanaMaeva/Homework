931d8393c88a19605bd6a8429caa76934c93fbc5
SvetlanaMaeva <mystik20009@gmail.com>
2022-07-12 13:01:44 +0300

    Task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,25 +1,29 @@
-﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
 
-// [3, 7, 23, 12] -> 19
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// [-4, -6, 89, 6] -> 0
+System.Console.WriteLine("Давай, для начала, зададим числа по условию. ");
+System.Console.Write("b1 = ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("k1 = ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("b2 = ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("k2 = ");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
-int[] numberArray = new int[10];
+double x = (b2 - b1) / (k1 - k2);
 
-int size = numberArray.Length;
-
-int result = 0;
-
-Console.Write("Массив: ");
-for (int i = 0; i < size; i++)
+if ((b1 == b2) && (k1 == k2))
 {
-    numberArray[i] = new Random().Next(1, 10);
-    
-    if (i % 2 != 0)
-    {
-        result += numberArray[i];
-    }
+    Console.WriteLine($"Прямые совпадают");
 }
-Console.WriteLine($"[{String.Join("; ", numberArray)}]");
-
-Console.WriteLine($"Cуммa элементов, стоящих на нечётных позициях: {result}");
+else if (((b1 == b2) && (k1 != k2)) || ((b1 != b2) && (k1 == k2)))
+{
+    Console.WriteLine($"Прямые параллельны");
+}
+else
+{
+    System.Console.WriteLine($"Точка пересечения: ({(-b1 * k2 + k1 * b2)/(k1-k2)}; {x})");
+}
