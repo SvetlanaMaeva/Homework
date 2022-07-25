d6fe953af3974ed701a621bff3cd31f8c08673a8
SvetlanaMaeva <mystik20009@gmail.com>
2022-07-25 15:29:44 +0300

    Task 66

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,45 +1,16 @@
-﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-// 17 -> такого числа в массиве нет
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
-Console.WriteLine("Введи количество строчек: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введи количество столбцов: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Какое число будем проверять? ");
-int verificationNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи начальное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи конечное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix = new int[rows, columns];
-
-for (int i = 0; i < matrix.GetLength(0); i++)
+int PrintNumber(int start, int end)
 {
-
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        matrix[i, j] = new Random().Next(1, 11);
-        Console.Write(matrix[i, j] + "\t");
-    }
-    Console.WriteLine();
+    if (start == end) return start;
+    return (start + PrintNumber(start+1, end));
 }
-
-void CheckForavAilability(int[,] array)
-{
-    bool existsElement = false;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (matrix[i, j] == verificationNumber)
-            {
-                Console.WriteLine($"Позиция введённого элемента {verificationNumber} -> matrix[{i},{j}]");
-                existsElement = true;
-            }
-        }
-    }
-    if (existsElement == false) Console.WriteLine("Такого элемента нет :(");
-}
-CheckForavAilability(matrix);
+Console.WriteLine(PrintNumber(M, N));
